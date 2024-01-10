9959ffe3acc90d78883fc87fc854bc6a3f9781e7
Abhay Lodhi <156104058+vabhayv@users.noreply.github.com>
2024-01-11 01:59:58 +0530

    Update restaurant.py
    being more clear about instrunctions;)

--- restaurant.py
+++ restaurant.py
@@ -1,6 +1,6 @@
 input("Do You Want To Split The Restaurant Bill\nTap enter to continue\n")
 Total = float(input("Total Amount of Bill?\n"))
-Tip = float(input("Do you want to tip the waiter? Write down the amount you want to tip, If not then write 0.\n"))
+Tip = float(input("Do you want to tip the waiter? Write down the amount you want to tip, If not then write 0.(Don't write the % sign or anything other than number!!)\n"))
 People = int(input("In how many peoples you want to split the bill?\n"))
 Final_amt = Total + Tip
 Per_head = (Final_amt/People)
